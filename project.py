32fd0986b35f3effe52331ad6dd1e8b9332de4e2
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2023-12-11 02:41:34 +0700

    chore: minor adjustment and param tweaking

--- project.py
+++ project.py
@@ -1,121 +1,144 @@
-# Python Footbal Face Recognition
-# Naufal Hardiansyah / 2540117855
-# Raymond Prasetio / 2501996846
-
 import cv2
 import os
 import numpy as np
 import math
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score 
+from sklearn.metrics import accuracy_score
 
-# Constants
-SCALE_FACTOR = 1.2
-MIN_NEIGHBORS = 5
- # https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
-FACE_CASCADE = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-FACE_RECOGNIZER = cv2.face.LBPHFaceRecognizer.create()
-DATASET_PATH = 'Dataset/'
-
-def preprocess(img):
-    # Apply image preprocessing techniques
-    img = cv2.resize(img, (150, 150))  # Resize the image
-    img = cv2.equalizeHist(img)  # Equalize histogram for improved contrast
-    img = cv2.GaussianBlur(img, (5, 5), 0)  # Apply Gaussian blur
-    img = cv2.bilateralFilter(img, 5, 150, 150)  # Apply bilateral filter
-    return img
-
-def trainAndTest():
-    print("Training and Testing Model")
-    face_list = [] #stores face
-    class_list = [] #stores the name
-    
-
-    # Initialize a list to store the predicted labels
-    names = os.listdir(DATASET_PATH)
-    for idx, name in enumerate(names):
-        full_path = DATASET_PATH + '/' + name
-        for img_name in os.listdir(full_path):
-            image_path = full_path + '/' + img_name
-            img = cv2.imread(image_path, 0)
-            img = preprocess(img)
-
-            # Detect face
-            # scaleFactor = everytime the image is scanned, its size will decrease by 20%
-            # minNeighbors = the amount of neighbors that 
-            detected_face = FACE_CASCADE.detectMultiScale(img, scaleFactor = SCALE_FACTOR, minNeighbors = MIN_NEIGHBORS)
-            if len(detected_face) < 1:
-                continue
-
-            for face in detected_face:
-                x, y, h, w = face
-                face_img = img[y:y+h, x:x+w]            
-                face_list.append(face_img)
-                class_list.append(idx)
-    
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(
-        face_list, 
-        class_list, 
-        test_size = 0.25, 
-        random_state = 0,
-        stratify = class_list
-    )
-    
-    # Train the recognizer on the training data
-    FACE_RECOGNIZER.train(X_train, np.array(y_train))
-    
-    # Test the recognizer on the test data
-    predicted_labels = []
-    for test_img in X_test:
-        res, confidence = FACE_RECOGNIZER.predict(test_img)
-        predicted_labels.append(res)
-
-    # Calculate accuracy
-    accuracy = accuracy_score(y_test, predicted_labels)
-    print(f"Accuracy: {accuracy * 100:.2f}%") 
-
-def predict():
-    print("Predicting")
-    predict_path = input("Input absolute path of the image to predict: ")
-    predict_img = cv2.imread(predict_path, 0)
-    # predict_img = preprocess(predict_img)
-    detected_face = FACE_CASCADE.detectMultiScale(predict_img, scaleFactor = SCALE_FACTOR, minNeighbors = MIN_NEIGHBORS)
-    names = os.listdir(DATASET_PATH)
+class FaceRecognition:
+    def __init__(self):
+        # Initialize the parameters
+        self.SCALE_FACTOR = 1.2
+        self.MIN_NEIGHBORS = 5
+        self.DATASET_PATH = 'Dataset/'
+        self.GAUSSIAN_BLUR = lambda img: cv2.GaussianBlur(img, (13, 13), 0)
+        self.BILATERAL_FILTER = lambda img: cv2.bilateralFilter(img, 10, 100, 100)
+        self.LABELS = os.listdir(self.DATASET_PATH)
         
-    if len(detected_face) < 1:
-        print("No Face Detected")
-        return
-    
-    for face in detected_face:
-        x, y, h, w = face
-        face_img = predict_img[y:y+h, x:x+w]    
-        res, confidence = FACE_RECOGNIZER.predict(face_img)
-        confidence = math.floor(confidence * 100) / 100
-        
-        #Draws a rectangle at the face
-        img_bgr = cv2.imread(predict_path)
-        cv2.rectangle(img_bgr, (x, y), (x+w, y+h), (0,255,0), 1)
-        text = names[res] + " : " + str(confidence) + "%"
-        cv2.putText(img_bgr, text,(x, y-10), cv2.FONT_HERSHEY_PLAIN, 1.5, (0,255,0), 0)
-        cv2.imshow('Result', img_bgr)
-        cv2.waitKey(0)        
-        
+        # Initialize the face cascade classifier and face recognizer        
+        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.face_recognizer = cv2.face.LBPHFaceRecognizer.create()
 
-# Main program
-# https://stackoverflow.com/questions/419163/what-does-if-name-main-do
+    def preprocess(self, img):
+        # Apply Gaussian blur
+        img = self.GAUSSIAN_BLUR(img)
+
+        # Apply Bilateral filter
+        img = self.BILATERAL_FILTER(img)
+        return img
+
+    def train_and_test(self):
+        print("Training and Testing Model")
+        face_list = []  # stores face
+        class_list = []  # stores the name
+
+        # Initialize a list to store the predicted labels
+        for idx, name in enumerate(self.LABELS):
+            full_path = self.DATASET_PATH + '/' + name
+            for img_name in os.listdir(full_path):
+                image_path = full_path + '/' + img_name
+                img = cv2.imread(image_path, 0)
+                img = self.preprocess(img)
+
+                # Detect face
+                detected_face = self.face_cascade.detectMultiScale(
+                    img, 
+                    scaleFactor=self.SCALE_FACTOR,
+                    minNeighbors=self.MIN_NEIGHBORS
+                )
+                if len(detected_face) < 1:
+                    continue
+
+                for face in detected_face:
+                    x, y, h, w = face
+                    face_img = img[y:y + h, x:x + w]
+                    face_list.append(face_img)
+                    class_list.append(idx)
+
+        # Split the data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(
+            face_list,
+            class_list,
+            test_size=0.25,
+            random_state=0,
+            stratify=class_list
+        )
+
+        # Train the recognizer on the training data
+        self.face_recognizer.train(X_train, np.array(y_train))
+
+        # Test the recognizer on the test data
+        predicted_labels = []
+        for test_img in X_test:
+            res, confidence = self.face_recognizer.predict(test_img)
+            predicted_labels.append(res)
+
+        # Calculate accuracy of the model
+        accuracy = accuracy_score(y_test, predicted_labels)
+        print(f"Accuracy: {accuracy * 100:2f}%")
+        
+        # Save the model
+        self.face_recognizer.save('model.xml')
+
+    def predict(self):
+        # Check if the model exists
+        if not os.path.exists('model.xml'):
+            print("Model not found. Please train the model first.")
+            return
+        
+        # Load the model
+        self.face_recognizer.read('model.xml')
+        
+        # Predict the image        
+        predict_path = input("Input absolute path of the image to predict: ")
+        predict_img = cv2.imread(predict_path, 0)
+        # Preprocessing disabled for predicting
+        # It gives 0% confidence despite predicting the correct result ¯\_(ツ)_/¯
+        # predict_img = self.preprocess(predict_img)
+        
+        detected_face = self.face_cascade.detectMultiScale(
+            predict_img, 
+            scaleFactor=self.SCALE_FACTOR,
+            minNeighbors=self.MIN_NEIGHBORS
+        )
+        
+        if len(detected_face) < 1:
+            print("No Face Detected")
+            return
+
+        for face in detected_face:
+            x, y, h, w = face
+            face_img = predict_img[y:y + h, x:x + w]
+            res, confidence = self.face_recognizer.predict(face_img)
+            print(res, confidence)
+            confidence = math.floor(confidence * 100) / 100
+
+            # Draws a rectangle at the face
+            img_bgr = cv2.imread(predict_path)
+            cv2.rectangle(img_bgr, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            text = self.LABELS[res] + " : " + str(confidence) + "%"
+            cv2.putText(img_bgr, text, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 0)
+            cv2.imshow('Result', img_bgr)
+            cv2.waitKey(0)
+
 if __name__ == "__main__":
+    face_recognition = FaceRecognition()
     while True:
-        print("Python Footbal Face Recognition")
+        print("Python Football Face Recognition")
         print("1. Train and Test Model")
         print("2. Predict")
         print("3. Exit")
-        choice = input("Enter your choice: ")
-        if choice == '1':
-            trainAndTest()
-        elif choice == '2':
-            predict()
-        elif choice == '3':
-            break
-        else:
-            print("Invalid Choice")
+        try:
+            choice = int(input("Enter your choice: "))
+            if choice == 1:
+                face_recognition.train_and_test()
+            elif choice == 2:
+                face_recognition.predict()
+            elif choice == 3:
+                break
+            else:
+                print("Invalid Choice")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+        except Exception as e:
+            print("An error occurred:", str(e))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-true_label
+LABELS
